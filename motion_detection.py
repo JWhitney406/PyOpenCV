5af12137b71e75ab7c22fc51c80901a1f0da0d6c
Sam Shissler <samshissler@gmail.com>
2017-02-22 17:07:35 -0700

    finished the color tracking

--- motion_detection.py
+++ motion_detection.py
@@ -10,9 +10,22 @@ last_frame = cv2.resize(last_frame,(x, y), interpolation = cv2.INTER_CUBIC)
 last_frame = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
 UPPER_THRESH = 50
 LOWER_THRESH = 50
+global lowClick, highClick
+lowClick = np.array([0,0,0])
+highClick = np.array([10,10,10])
+firstTime = True
 
-
+COLOR_THRESH = 10
 kernel = np.ones((3,3),np.uint8)
+
+def getColor(event, y, x, flags, param):
+    global lowClick, highClick
+    if event == cv2.EVENT_LBUTTONDOWN:
+        lowClick = np.array([hsv[x][y][0]-15, hsv[x][y][1]-15, hsv[x][y][2]-15])
+        highClick = np.array([hsv[x][y][0]+15, hsv[x][y][1]+15, hsv[x][y][2]+15])
+        print("Mouse clicked")
+        print("lower:", lowClick)
+        print("highter:", highClick)
 
 while(True):
     frame = cap.read()[1]
@@ -20,14 +33,20 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     motion = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    hsvObject = cv2.inRange(hsv, lowClick, highClick)
     gray = cv2.resize(gray,(x, y), interpolation = cv2.INTER_CUBIC)
     gray2 = cv2.resize(gray,(x, y), interpolation = cv2.INTER_CUBIC)
-
+    
     for i in range(0, y):
         for j in range(0, x):
+            # if abs(hsv[i][j][0] - clickColor[0]) > COLOR_THRESH & abs(hsv[i][j][1] - clickColor[1]) > COLOR_THRESH & abs(hsv[i][j][2] - clickColor[2]) > COLOR_THRESH:
+            #     hsvObject[i][j] = [255,255,255]
+            # else:
+            #     hsvObject[i][j] = [0,0,0]
+                
             blurred_pixel = int(int(last_frame[i, j])*.8 + int(gray[i, j])*.2)
             motion_pixel = min(255, 2*int(abs(int(gray[i, j])-int(last_frame[i, j]))))
+            
             if motion_pixel > UPPER_THRESH:
                 motion_pixel = 255
             elif motion_pixel < LOWER_THRESH:
@@ -56,10 +75,16 @@ while(True):
 #    cv2.imshow('edges',cv2.resize(edges,(x*5, y*5), interpolation = cv2.INTER_CUBIC)) 
     cv2.imshow('Contours',cv2.resize((255-cont),(x*5, y*5), interpolation = cv2.INTER_CUBIC))
     cv2.imshow('Original', cv2.resize(frame,(x*5, y*5), interpolation = cv2.INTER_CUBIC))
+    if firstTime:
+        cv2.setMouseCallback('hsv', getColor)
+        firstTime = False
 
+    cv2.imshow('HSV Object', cv2.resize(hsvObject,(x*5, y*5), interpolation = cv2.INTER_CUBIC))
+                
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
